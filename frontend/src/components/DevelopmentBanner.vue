6313a546ad1f1f782094a43e5a29671cb5cf9697
ryankupk <ryankupk@gmail.com>
2024-02-26 03:14:57 +0000

    update some styles

--- frontend/src/components/DevelopmentBanner.vue
+++ frontend/src/components/DevelopmentBanner.vue
@@ -1,20 +1,30 @@
+<script setup>
+
+</script>
+
+
 <template>
   <span class="development-banner">&#9888; Site is currently under active development, expect changes &#9888;</span>
 </template>
 
-<script lang="ts">
-export default {
-  name: 'DevelopmentBanner',
-}
-</script>
 
 <style scoped>
-.development-banner {
+  .development-banner {
+
+    width: 100%; /* Ensures the banner stretches across the viewport */
+    /* max-width: 500px; */
+    margin: 0 auto;
+    text-align: center;
+    position: sticky;
+    top: 20;
+    left: 0;
+    z-index: 1000;
     background-color: #ffecb3; /* Mellow yellow background */
     color: black; /* Black text color */
-    padding: 10px 20px; /* Adequate padding for spacing */
-    text-align: center; /* Center the text and icon */
+    padding: 10px 20px; /* Adds padding for some spacing */
+    box-sizing: border-box; /* Ensures padding does not affect the overall width */
+    text-align: center; /* Centers the text */
     font-weight: bold; /* Make the text stand out */
-    border-bottom: 2px solid #ffd700; /* A subtle border to define the banner's bottom edge */
+    border-bottom: 4px solid #ffd700; /* A subtle border to define the banner's bottom edge */
   }
 </style>
