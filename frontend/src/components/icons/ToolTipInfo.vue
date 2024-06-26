6e8461de2c1cf0885535ff680f0b16229c4eab00
ryankupk <ryankupk@gmail.com>
2024-06-26 23:37:52 +0000

    create component with circled lowercase 'i' svg with tippy

---
+++ frontend/src/components/icons/ToolTipInfo.vue
@@ -0,0 +1,15 @@
+
+<script setup>
+import { Tippy } from 'vue-tippy';
+import 'tippy.js/dist/tippy.css'
+
+</script>
+
+<template>
+    <tippy placement="" content="" allow-h-t-m-l=true>
+        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20">
+            <circle cx="10" cy="10" r="9" fill="none" stroke="currentColor" stroke-width="2"/>
+            <text x="10" y="15" font-family="Arial" font-size="14" text-anchor="middle" fill="currentColor">i</text>
+        </svg>
+    </tippy>
+</template>
