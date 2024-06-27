bdb96b571ca14f15bc40885eb0278d91d87d5d22
ryankupk <ryankupk@gmail.com>
2024-06-27 00:12:49 +0000

    fix warning for tooltips

--- frontend/src/components/icons/ToolTipInfo.vue
+++ frontend/src/components/icons/ToolTipInfo.vue
@@ -6,7 +6,7 @@ import 'tippy.js/dist/tippy.css'
 </script>
 
 <template>
-    <tippy placement="" content="" allow-h-t-m-l=true>
+    <tippy placement="" content="" allow-h-t-m-l>
         <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20">
             <circle cx="10" cy="10" r="9" fill="none" stroke="currentColor" stroke-width="2"/>
             <text x="10" y="15" font-family="Arial" font-size="14" text-anchor="middle" fill="currentColor">i</text>
